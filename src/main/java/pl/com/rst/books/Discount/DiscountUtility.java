0e54a4f1b2529edb25947cab49cea903cbc8cceb
Marcin Sobejko <sobejko.marcin@gmail.com>
2018-08-10 14:22:18 +0200

    Refactor DiscountUtility.java to fir Utils template better

--- src/main/java/pl/com/rst/books/Discount/DiscountUtility.java
+++ src/main/java/pl/com/rst/books/Discount/DiscountUtility.java
@@ -9,21 +9,21 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class DiscountUtility {
+public final class DiscountUtility {
 
     private static final float ORDER_PRICE_TRESHOLD_FOR_LARGE_ORDER = 300;
 
-    public long getDiscount(String bookId, float orderPrice, String discountCode, Set<DiscountType> availableDiscounts) throws BookNotFoundException {
+    private DiscountUtility() {
+        // Not used in utils class
+    }
+
+    public static long getDiscount(String bookId, float orderPrice, String discountCode, Set<DiscountType> availableDiscounts) throws BookNotFoundException {
         final Book book = getBookRepository().getBook(Long.valueOf(bookId));
 
         return (long) availableDiscounts.stream().mapToDouble(d -> d.calculate(book, orderPrice, discountCode)).sum();
     }
 
-    public boolean isArrayEmpty(Object[] array) {
-        return array == null || array.length == 0;
-    }
-
-    public BookRepository getBookRepository() {
+    public static BookRepository getBookRepository() {
         return new BookRepository();
     }
 }
